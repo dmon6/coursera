8599e457e2a87c12b6fa0ba7d5ba7a1dc71d72fb
Dennis Monton <dmonton66@gmail.com>
2020-01-28 21:24:16 -0500

    added 'requests' module and updated week1 labs

--- course_2_python_interact_operating_system/week1/week1_prac_log.py
+++ course_2_python_interact_operating_system/week1/week1_prac_log.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3.7
 
+#!/usr/bin/env python3
+from network import *
 import shutil
 import psutil
 
 
 def check_disk_usage(disk):
+    """Verifies that there's enough free space on disk"""
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
-    print(f'Percentage of free space: {round(free, 2)}%')
     return free > 20
 
 
 def check_cpu_usage():
+    """Verifies that there's enough unused CPU"""
     usage = psutil.cpu_percent(1)
     return usage < 75
 
+# If there's not enough disk, or not enough CPU, print an error
+
 
 if not check_disk_usage('/') or not check_cpu_usage():
-    print('ERROR')
+    print("ERROR!")
+elif check_localhost() and check_connectivity():
+    print("Everything ok")
 else:
-    print('Everything is OK!')
+    print("Network checks failed")
